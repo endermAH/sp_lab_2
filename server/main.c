7d1de85e528ef51e46285e452e1ab6ffeb85d34a
John <gerakl00@gmail.com>
2019-11-07 01:56:25 +0300

    Basic uganda server

--- server/main.c
+++ server/main.c
@@ -15,9 +15,14 @@ struct globalArgs_t {
   int portno;
 } globalArgs;
 
+void error(char* msg) {
+  fprintf(stderr, msg);
+  exit(1);
+}
+
 void displayUsage() {
   printf("\nUSAGE:\n%s [-h] [-p <port number>]\n\nARGS: \n-p Port number to listen\n-h: Help\n\n", name);
-  return 1;
+  exit(0);
 }
 
 int getStartData(int argc, char** argv) {
@@ -44,19 +49,43 @@ int getStartData(int argc, char** argv) {
     fprintf(stderr, "Incorrect port number\n");
     return 0;
   }
+  if (globalArgs.portno == NULL) {
+    fprintf(stderr, "Can not run without port number\n");
+    return 0;
+  }
 
   return 1;
 }
 
 int main(int argc, char *argv[]) {
-  int sockfd; //Socket descriptor
+  int listenfd; //Socket descriptor
   int portno; //Port number
   struct sockaddr_in serv_addr; //IP struct
   struct hostnet *server;
 
   char buffer[256];
 
-  if (!getStartData(argc, argv)) return 1; //Stop server with wrong input
+  if (!getStartData(argc, argv)) exit(1); //Stop server with wrong input
 
+  //Creating socket
+  listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_UDP);
+  if (sockfd < 0) error("Can not open socket");
 
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(5000);
+
+  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); //Binding socket
+
+  listen(listenfd, 10);
+
+  while(1) {
+    connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+
+    snprintf(sendBuff, sizeof(sendBuff), "DAROVA VI POPALI V UGANDU\r\n")
+    write(connfd, sendBuff, strlen(sendBuff));
+
+    close(connfd);
+    sleep(1);
+ }
 }
