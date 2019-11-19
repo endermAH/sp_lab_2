11d3f81eb6db2b6f75e8326d96176a220801b2ea
John <gerakl00@gmail.com>
2019-11-19 12:56:32 +0300

    Workable server snils validate

--- server/main.c
+++ server/main.c
@@ -9,13 +9,14 @@
 #include <sys/types.h>
 
 const char *optString = "p:h";
+const int BUFFER_LENGTH = 256;
 
 struct globalArgs_t {
   int portno;
 } globalArgs;
 
 void error(char* msg) {
-  fprintf(stderr, msg);
+  perror(msg);
   exit(1);
 }
 
@@ -57,34 +58,57 @@ int getStartData(int argc, char** argv) {
 }
 
 int main(int argc, char *argv[]) {
-  int listenfd, connfd; //Socket descriptors
+  int listenfd; //Socket descriptors
   int portno; //Port number
+  socklen_t struct_len = sizeof(struct sockaddr_in);
   struct sockaddr_in serv_addr; //IP struct
-  struct hostnet *server;
+  struct sockaddr from;
 
-  char buffer[256];
+  char buffer[BUFFER_LENGTH];
 
   if (!getStartData(argc, argv)) exit(1); //Stop server with wrong input
 
   //Creating socket
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  listenfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   if (listenfd < 0) error("Can not open \n");
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   serv_addr.sin_port = htons(globalArgs.portno);
 
-  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); //Binding socket
+   //Binding socket
+  int test = bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-  listen(listenfd, 10);
-
+  int counter;
+  short is_SNILS;
+  //char response[1];
   while(1) {
-    connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+    memset(buffer, 0, BUFFER_LENGTH);
+    int count = recvfrom(listenfd, buffer, BUFFER_LENGTH - 1, 0, &from, &struct_len);
+    counter = 0;
+    is_SNILS = 1;
+    for (int i = 0; i < count; i++) {
+      if (buffer[i] == ' ' || buffer[i] == '-') {
+        if (buffer[i-1] != ' ' && buffer[i-1] != '-') continue;
+        else {
+          is_SNILS = 0;
+        }
+      }
+      if (buffer[i] >= 48 && buffer[i] <= 57) {
+        counter++;
+      } else {
+        is_SNILS = 0;
+      }
+    }
 
-    snprintf(buffer, sizeof(buffer), "DAROVA VI POPALI V UGANDU\r\n");
-    write(connfd, buffer, strlen(buffer));
+    if (counter != 11) is_SNILS = 0;
+    if (is_SNILS == 0) {
+      int test = sendto(listenfd, "no", 2, 0, &from, struct_len);
+    } else {
+      int test = sendto(listenfd, "yes", 3, 0, &from, struct_len);
+    }
+    printf("Accepted: \"%s\"!\nSend status: %d \n", buffer, test);
 
-    close(connfd);
     sleep(1);
  }
 }
